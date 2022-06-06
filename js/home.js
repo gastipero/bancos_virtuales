bbcbadc0669717ffbae43a2ad8465725154fb6c6
Gaston Pero <gastipero@gmail.com>
2022-06-06 18:46:40 -0300

    Se termina la parte funcional

--- js/home.js
+++ js/home.js
@@ -12,6 +12,7 @@ let generador_token
 let btnAgregarCuenta
 let divAdministrarCuentas
 let divInversiones
+let divModUsuario
 const nav_bar = document.querySelector("#opciones")
 let nav_li = []
 for (let i = 0; i < nav_bar.firstElementChild.children.length; i+=1) {
@@ -42,43 +43,7 @@ nav_container.addEventListener ('mouseout',() => {
     nav_container.classList = 'navigator'
 })
 
-function administrarCuentas(op2) {
-    console.log(`>1.Agregar cuenta. 
->2.Resumen de cuentas. 
->3.Eliminar cuenta.
->4.Ver cuenta
->7.Volver.`)
-    /* op2= prompt(`Seleccione una opción.`) */
-    switch (op2) {
-        case "1":
-            agregarCuenta()
-            console.log("Cuenta agregada correctamente")
-            menuPrincipal()
-            break;
-        case "2":
-            for (let index = 0; index < cuentas.length; index++) {
-                console.log(cuentas[index]);
-            }
-            menuPrincipal()
-            break;
-        case "3":
-            eliminarCuenta()
-            break;
-        case "4":
-            for (let i = 0; i < cuentas.length; i++) {
-                console.log(cuentas[i].banco);
-            };
-            banco_filtro= prompt("¿Qué cuenta desea ver?");
-            banco_filtrado = cuentas.filter(x => x.banco == banco_filtro);
-            console.log(banco_filtrado)
-            break;    
-        case "7":
-            menuPrincipal()
-            break;
-
-        default:
-            break;
-    }
+function administrarCuentas() {
     cont_body.innerHTML = "<li><ul>Agregar cuenta</ul><ul>Resumen de cuentas</ul></li>"
     cont_body.firstChild.classList="nav-cuentas"
     let ul_cuentas = cont_body.firstChild.children
@@ -134,39 +99,6 @@ function pagoServicios () {
     console.log("funcion en desarrollo")
 }
 
-function modificarUsuario (usuario) { 
-    if (usuario!="") {
-        /* agregar validación para ingresar usuario anterior */
-        usuario=prompt("Ingrese nuevo usuario")
-        localStorage.setItem("usuario_nuevo",usuario)
-        usuario1 = localStorage.getItem("usuario_nuevo")
-        Swal.fire({
-            title: 'Éxito!',
-            text: 'Usuario modificado exitosamente',
-            icon: 'success',
-            confirmButtonText: 'Ok'
-          })
-        location.href = "../index.html"
-    }
-    else {console.log("Ingrese al menos un caracter")}
-}
-function modificarContrasena (contrasena) { 
-    if (contrasena!="") {
-        /* agregar validación para ingresar contraseña anterior */
-        contrasena=prompt("Ingrese nueva contraseña")
-        localStorage.setItem("contrasena_nueva",contrasena)
-        contrasena1=localStorage.getItem("contrasena_nueva")
-        Swal.fire({
-            title: 'Éxito!',
-            text: 'Contraseña modificada exitosamente',
-            icon: 'success',
-            confirmButtonText: 'Ok'
-          })
-        location.href = "../index.html"
-    }
-    else {console.log("Ingrese al menos un caracter")}
-}
-
 function salir () {
     location.href = "../index.html"
 }
@@ -195,7 +127,7 @@ let agregarCuenta = () => {
     if(cont_body.children[2]){
         cont_body.children[2].remove()
     }
-    divAdministrarCuentas.innerHTML='<form id="confirmaAgregarCuenta"><label for="agregar-banco">Banco</label><input type="text" name="agregar-banco" id="agregar-banco"><label for="agregar-alias">Alias</label><input type="text" name="agregar-alias" id="agregar-alias"><label for="agregar-saldo">Saldo</label><input type="text" name="agregar-saldo" id="agregar-saldo"><button type="submit">Ingresar</button><button type="reset">Borrar</button></form>'
+    divAdministrarCuentas.innerHTML='<form id="confirmaAgregarCuenta"><label for="agregar-banco" class="col-6 my-1">Banco</label><input type="text" name="agregar-banco" class="col-6 my-1" id="agregar-banco"><label class="col-6 my-1" for="agregar-alias">Alias</label><input type="text" name="agregar-alias" class="col-6 my-1" id="agregar-alias"><label for="agregar-saldo" class="col-6 my-1">Saldo</label><input type="text" name="agregar-saldo" class="col-6 my-1" id="agregar-saldo"><button type="submit" class="col-4 offset-2 mt-2">Ingresar</button><button type="reset" class="col-4 mt-2">Borrar</button></form>'
     
     formAgregarCuenta = document.querySelector("#confirmaAgregarCuenta")
     function agregarBanco(evt){
@@ -265,25 +197,86 @@ function limpiarAdminCuenta() {
     }
 }
 
-function modificar(opModificar) {
-    console.log("1. Modificar Usuario - 2. Modificar contraseña - 7. Volver al Menú principal")
-    opModificar=prompt("Qué desea modificar?")
-    switch (opModificar) {
-        case "1":
-            modificarUsuario()
-            break;
-        case "2":
-            modificarContrasena()
-            break;
-        case "7":
-            menuPrincipal()
-            break;
-        default:
-            alert("Ingrese opcion válida")
-            modificar()
-            break;
+function modificar() {
+    cont_body.innerHTML = "<li><ul>Modifcar usuario</ul><ul>Modificar contraseña</ul></li>"
+    cont_body.firstChild.classList="nav-modcuentas"
+    let ul_mod_cuentas = cont_body.firstChild.children
+    for (const item of ul_mod_cuentas) {
+        item.classList= "item-mod-cuenta"
     }
+    ul_mod_cuentas[0].addEventListener ('click', () => {/* limpiarModCuenta(), */modificarUsuario()})
+    ul_mod_cuentas[1].addEventListener ('click', () => {/* limpiarModCuenta(), */modificarContrasena()})
+    divModUsuario = document.createElement ('div')
+    divModUsuario.classList = 'modificar-usuario'
+    cont_body.appendChild(divModUsuario)
 }
+
+function modificarUsuario () { 
+    divModUsuario.innerHTML = '<form id="formModificarUsuario"><label for="modUsuario">Nuevo usuario</label><input type="text" name="modUsuario" id="modUsuario"><button type="submit">Modificar</button>'
+    let inputUsuario = document.querySelector("#modUsuario")
+    let formUsuario = document.querySelector("#formModificarUsuario")
+    formUsuario.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        if(!inputUsuario.value) {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Debe ingresar al menos un caracter',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+              })
+        }
+        else {
+            localStorage.setItem("usuario_nuevo",inputUsuario.value)
+            usuario1=inputUsuario.value
+            inputUsuario.value = ""
+            Swal.fire({
+                title: 'Éxito!',
+                text: 'Usuario modificado exitosamente',
+                icon: 'success',
+                confirmButtonText: 'Ok'
+              })
+              setTimeout(()=> {location.href = "../index.html"}, 2000)
+        }
+    }
+    )
+}
+
+function modificarContrasena (contrasena) { 
+    divModUsuario.innerHTML = '<form id="formModificarContrasena"><label for="modContrasena">Nueva contraseña</label><input type="text" name="modContrasena" id="modContrasena"><button type="submit">Modificar</button>'
+    let inputContra = document.querySelector("#modContrasena")
+    let formContra = document.querySelector("#formModificarContrasena")
+    formContra.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        if(!inputContra.value) {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Debe ingresar al menos un caracter',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+              })
+        }
+        else {
+            localStorage.setItem("contrasena_nueva",inputContra.value)
+            contrasena1=inputContra.value
+            inputContra.value = ""
+            Swal.fire({
+                title: 'Éxito!',
+                text: 'Contraseña modificada exitosamente',
+                icon: 'success',
+                confirmButtonText: 'Ok'
+              })
+              setTimeout(()=> {location.href = "../index.html"}, 2000)
+        }
+    }
+    )
+}
+
+/* function limpiarModCuenta() {
+    if (document.querySelector(".administrar-cuenta")) {
+        let contenedor_admin_cuentas = document.querySelector(".administrar-cuenta")
+        contenedor_admin_cuentas.remove()
+    }
+} */
 
 
 /* Inversiones - API */
@@ -295,6 +288,7 @@ async function agregarCryptoBinance (simboloFiltrado) {
         .then ((res) => res.json())
         .then((result) => {
             result.forEach(simbolo => {
+                simbolo.id = Date.now()
                 if(simbolo.symbol == simboloFiltrado.toUpperCase()) {
                     newSymbol.push(simbolo)
                     console.log(newSymbol)
@@ -307,7 +301,7 @@ async function agregarCryptoBinance (simboloFiltrado) {
 
 }
 
-/* Intento hacer una actualización en los valores de la API */
+/* Actualización en los valores de la API */
 function peticionBinance () {
     let URL = `https://api.binance.com/api/v3/ticker/24hr`
     fetch(URL)
@@ -318,6 +312,7 @@ function peticionBinance () {
             result.forEach(crypto => {
                 SymbolsLS.forEach(cryptoLS => {
                     if (crypto.symbol == cryptoLS.symbol){
+                        crypto.id = cryptoLS.id
                         newCryptoLS.push(crypto)
                     }})
             });
@@ -330,28 +325,47 @@ function inversionCripto () {
     divInversiones = document.createElement ('div')
     divInversiones.classList = 'div-inversiones'
     cont_body.appendChild(divInversiones)
-    divInversiones.innerHTML =' <form id="formBuscaCrypto"><label for="buscarCrypto">Simbolo</label><input type="text" name="buscarCrypto" id="buscarCrypto"><button type="submit">Buscar</button>'
+    divInversiones.innerHTML =' <form id="formBuscaCrypto"><label for="buscarCrypto">Simbolo</label><input type="text" name="buscarCrypto" id="buscarCrypto"><button type="submit">Agregar</button>'
     let inputCrypto = document.querySelector("#buscarCrypto")
     let formCrypto = document.querySelector("#formBuscaCrypto")
     formCrypto.addEventListener('submit',encontrarCrypto)
-    setInterval(() => {
-        peticionBinance(),
-        limpiarEscribirCrypto(),
-        escribirCrypto()
-    }, 2000);
     if (localStorage.getItem ('Symbols')){
         SymbolsLS= JSON.parse(localStorage.getItem ('Symbols'))
         newSymbol=SymbolsLS
+        peticionBinance()
+        escribirCrypto()
     }
     function encontrarCrypto (evt) {
         evt.preventDefault()
+        let existe=false
         console.log(inputCrypto.value)
         cryptoBuscada = inputCrypto.value
-        agregarCryptoBinance(cryptoBuscada)
+        SymbolsLS.forEach(elemento => {
+            if (elemento.symbol == cryptoBuscada) {
+                existe = true
+            }             
+        });
+        if (!existe) {
+            agregarCryptoBinance(cryptoBuscada)
+        }
+        else {
+            Swal.fire({
+                title: 'Error',
+                text: 'Este par ya existe',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            })
+        }
+        
         inputCrypto.value=""
     }
-    peticionBinance()
-    escribirCrypto()
+    setInterval(() => {
+        if (localStorage.getItem ('Symbols')){
+            peticionBinance(),
+            limpiarEscribirCrypto(),
+            escribirCrypto()
+        }
+    }, 2000);
 }
 
 
@@ -361,16 +375,30 @@ function escribirCrypto () {
     let titulosCrypto = document.createElement('li')
     titulosCrypto.classList = 'titulos-crypto'
     divInversiones.appendChild(titulosCrypto)
-    titulosCrypto.innerHTML = `<h2>Simbolo</h2><h2>Último Precio</h2><h2>Cambio de precio 24hr</h2><h2>Cambio de precio 24hr (%)</h2>`
+    titulosCrypto.innerHTML = `<ul>Simbolo</ul><ul>Último Precio</ul><ul>Cambio de precio 24hr</ul><ul>Cambio de precio 24hr (%)</ul>`
     divCryptos = document.createElement ('div')
     divCryptos.classList = 'tabla-cryptos'
     divInversiones.appendChild(divCryptos)
     SymbolsLS.forEach(cryptomoneda => {
         let each_crypto = document.createElement ('li')
         each_crypto.classList = 'lista-cryptos'
+        each_crypto.id = cryptomoneda.id
         divCryptos.appendChild(each_crypto)
-        each_crypto.innerHTML = `<ul>${cryptomoneda.symbol}</ul><ul>${cryptomoneda.lastPrice}</ul><ul>${cryptomoneda.priceChange}</ul><ul>${cryptomoneda.priceChangePercent}%</ul>` 
+        each_crypto.innerHTML = `<ul>${cryptomoneda.symbol}</ul><ul>${cryptomoneda.lastPrice}</ul><ul>${cryptomoneda.priceChange}</ul><ul>${cryptomoneda.priceChangePercent}%</ul><i class="fas fa-times" id="delete1"></i>` 
     });
+    const eliminarCrypto = document.querySelectorAll("#delete1")
+    eliminarCrypto.forEach(cruz => {
+        cruz.addEventListener ('click',borrarCrypto)
+    }); 
+}
+
+function borrarCrypto (evt) {
+    evt.preventDefault()
+    console.log(evt.target.parentElement.id)
+    const idCrypto=evt.target.parentElement.id
+    SymbolsLS = SymbolsLS.filter (simFil => simFil.id != idCrypto)
+    localStorage.setItem('Symbols',JSON.stringify(SymbolsLS))    
+    newSymbol=SymbolsLS
 }
 
 function limpiarEscribirCrypto() {
